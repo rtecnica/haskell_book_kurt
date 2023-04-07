b5eb1dd4f2702632ded5874bdc62c7aa1c81421e
ijmaldonado <imaldonado@wiseconn.com>
2023-04-07 14:09:58 -0400

    Added Lesson_10

---
+++ Unit_1/Lesson_10/capstone.hs
@@ -0,0 +1,34 @@
+robot (name, attack, hp) = \message -> message (name, attack, hp)
+
+name (n,_,_) = n
+attack (_,a,_) = a
+hp (_,_,hp) = hp
+
+getName aRobot = aRobot name
+getAttack aRobot = aRobot attack
+getHP aRobot = aRobot hp
+
+setName aRobot newName = aRobot (\(n,a,h) -> robot (newName,a,h))
+setAttack aRobot newAttack = aRobot (\(n,a,h) -> robot (n,newAttack,h))
+setHP aRobot newHP = aRobot (\(n,a,h) -> robot (n,a,newHP))
+
+printRobot aRobot = aRobot (\(n,a,h) -> n ++
+    " attack:" ++ show a ++
+    " hp:"++ show h)
+
+damage aRobot attackDamage = aRobot (\(n,a,h) ->
+    robot (n,a,h-attackDamage))
+
+fight aRobot defender = damage defender attack
+    where attack = if getHP aRobot > 10
+            then getAttack aRobot
+            else 0
+
+getHPs = map getHP
+
+nRoundsFightSim fastRobot slowRobot rounds =
+    if rounds  == 0
+        then (fastRobot, slowRobot)
+    else
+         (fastRobot, slowRobot)
+        --nRoundsFightSim (fight fastRobot slowRobot) (fight slowRobot fastRobot) (rounds - 1)
---
+++ Unit_1/Lesson_9/HO_funcs.hs
@@ -0,0 +1,13 @@
+import Data.Char
+-- Q9.1 Use 'filter' and 'length' to recreate elem
+myElem item list = 
+    length (filter (item ==) list) >= 1
+
+-- Q9.2 
+isPalindrome candidate = 
+    processed_candidate == reverse processed_candidate 
+    where processed_candidate = map toLower (filter (' ' /= ) candidate)
+
+-- Q9.3
+harmonic n = 
+    foldl (+) 0 (map (1/) [1..n])
