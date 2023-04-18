57cd7bf618d50760cfddcd438ada8897f855b216
imaldonado <imaldonado@wiseconn.com>
2023-04-18 11:01:38 -0400

    Update

---
+++ Unit_2/Lesson_14/my_type_classes.hs
@@ -0,0 +1,16 @@
+-- Q14.1
+data SixSidedDie = S1 | S2 | S3 | S4 | S5 | S6 deriving (Enum)
+instance Eq SixSidedDie where
+    (==) x y = fromEnum x == fromEnum y
+
+instance Ord SixSidedDie where
+    compare x y = compare (fromEnum x) (fromEnum y)
+
+-- Q14.2
+data FiveSidedDie = F1 | F2 | F3 | F4 | F5 deriving (Eq, Ord, Enum)
+class (Ord a, Eq a, Enum a) => Die a where
+    rollDie :: a -> String 
+
+instance Die FiveSidedDie where
+    rollDie die = 
+        "No"
