914dc5d4a50d13d50918d846437b80de463afb42
rtecnica <imaldonado@ing.uchile.cl>
2023-04-28 23:51:03 -0400

    Update

--- Unit_4/Lesson_22/simple_calc.hs
+++ Unit_4/Lesson_22/simple_calc.hs
@@ -1,4 +1,5 @@
-import Data.Text (splitOn, pack, unpack, strip)
+import Data.Text (pack, splitOn, strip, unpack)
+
 -- Q22.1
 -- Write a program, simple_calc.hs, that reads simple equations involving adding
 -- two numbers or multiplying two numbers. The program should solve the equation each
@@ -7,22 +8,38 @@ import Data.Text (splitOn, pack, unpack, strip)
 main :: IO ()
 main = do
   userInput <- getContents
-  print (printExpr (parsExpr userInput))
+  mapM_ (print . printExpr . parsExpr) (split userInput)
 
 data Expr = Expr
   { args :: (Int, Int),
     operator :: Operator
   }
 
+-- Motherfucking lazy split
+split :: String -> [String]
+split [] = [""]
+split (c : cs)
+  | c == '\n' = "" : rest
+  | otherwise = (c : head rest) : tail rest
+  where
+    rest = split cs
+
+collectLine :: String -> String
+collectLine (x : xs) =
+  if x == '\n'
+    then ""
+    else x : collectLine xs
+
 xorBool :: Bool -> Bool -> Bool
 xorBool value1 value2 = (value1 || value2) && not (value1 && value2)
 
 evalExpr :: Expr -> Int
 evalExpr (Expr args operator) = case operator of
-                                  Sum -> firstarg + secondarg
-                                  Prod -> firstarg * secondarg
-                                  where firstarg = fst args
-                                        secondarg = snd args
+  Sum -> firstarg + secondarg
+  Prod -> firstarg * secondarg
+  where
+    firstarg = fst args
+    secondarg = snd args
 
 printExpr :: Maybe Expr -> String
 printExpr (Just expr) = show (evalExpr expr)
@@ -31,28 +48,34 @@ printExpr Nothing = "Invalid Expression"
 data Operator = Sum | Prod deriving (Show, Eq)
 
 checkExprType :: String -> Maybe Operator
-checkExprType expr = if xorBool is_sum is_prod
-                      then if is_sum
-                        then Just Sum
-                        else Just Prod
-                      else Nothing
-                    where is_sum = '+' `elem` expr
-                          is_prod = '*' `elem` expr
+checkExprType expr =
+  if xorBool is_sum is_prod
+    then
+      if is_sum
+        then Just Sum
+        else Just Prod
+    else Nothing
+  where
+    is_sum = '+' `elem` expr
+    is_prod = '*' `elem` expr
 
 parsExpr :: String -> Maybe Expr
-parsExpr expr =  case exprType of
-                    Just Sum -> if length splitsum == 2
-                            then Just (Expr parsedsum Sum)
-                            else Nothing
-                    Just Prod -> if length splitprod == 2
-                            then Just (Expr parsedprod Prod)
-                            else Nothing
-                    Nothing -> Nothing
-                  where exprType = checkExprType expr
-                        splitsum = splitOn (pack "+") (pack expr)
-                        splitprod = splitOn (pack "*") (pack expr)
-                        parsedsum = tuplefy (map (read . unpack) splitsum)
-                        parsedprod = tuplefy (map (read . unpack) splitprod)
+parsExpr expr = case exprType of
+  Just Sum ->
+    if length splitsum == 2
+      then Just (Expr parsedsum Sum)
+      else Nothing
+  Just Prod ->
+    if length splitprod == 2
+      then Just (Expr parsedprod Prod)
+      else Nothing
+  Nothing -> Nothing
+  where
+    exprType = checkExprType expr
+    splitsum = splitOn (pack "+") (pack expr)
+    splitprod = splitOn (pack "*") (pack expr)
+    parsedsum = tuplefy (map (read . unpack) splitsum)
+    parsedprod = tuplefy (map (read . unpack) splitprod)
 
 tuplefy :: [a] -> (a, a)
 tuplefy [x, y] = (x, y)
