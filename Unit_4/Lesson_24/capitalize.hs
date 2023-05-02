65db6e3549a3691fc44055e45c9f6b4345319a30
rtecnica <imaldonado@ing.uchile.cl>
2023-05-01 23:22:33 -0400

    Update

--- Unit_4/Lesson_24/capitalize.hs
+++ Unit_4/Lesson_24/capitalize.hs
@@ -1,1 +1,13 @@
 -- Q24.2
+
+import Data.Text qualified as T
+import Data.Text.IO qualified as TIO
+import System.Environment
+import System.IO
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let fileName = head args
+  source <- TIO.readFile fileName
+  TIO.writeFile fileName (T.toUpper source)
--- Unit_4/Lesson_24/cp.hs
+++ Unit_4/Lesson_24/cp.hs
@@ -1,2 +1,12 @@
--- 24.1
+-- Q24.1
 
+import System.Environment
+import System.IO
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let firstArg = head args
+  let secondArg = args !! 1
+  source <- readFile firstArg
+  writeFile secondArg source
