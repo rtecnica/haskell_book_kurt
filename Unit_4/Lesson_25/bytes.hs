4900598af12f8c1b0e415ec73b770ed757d831e9
imaldonado <imaldonado@wiseconn.com>
2023-05-03 21:19:46 -0400

    Update

--- Unit_4/Lesson_25/bytes.hs
+++ Unit_4/Lesson_25/bytes.hs
@@ -1,14 +1,17 @@
 import System.Environment
-import System.Random (randomRIO) 
+import System.Random (randomRIO)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
+import Control.Monad
+import GHC.Float (timesDouble)
 
 main :: IO ()
 main = do
     args <- getArgs
     let fileName = head args
     imageFile <- BC.readFile fileName
-    glitched <- randomReplaceByte imageFile
+    glitchedByte <- randomSortSection imageFile 5 
+    glitched <- randomReplaceByte glitchedByte 25
     let glitchedFileName = mconcat ["glitched_",fileName]
     BC.writeFile glitchedFileName glitched
     print "all done"
@@ -26,9 +29,30 @@ replaceByte loc charVal bytes = mconcat [before,newChar,after]
           after = BC.drop 1 rest
           newChar = intToBC charVal
 
-randomReplaceByte :: BC.ByteString -> IO BC.ByteString
-randomReplaceByte bytes = do
+randomReplaceByte :: BC.ByteString -> Int -> IO BC.ByteString
+randomReplaceByte bytes times = do
     let bytesLength = BC.length bytes
-    location <- randomRIO (1,bytesLength)
-    charVal <- randomRIO (0,255)
-    return (replaceByte location charVal bytes)
+    location <- replicateM times (randomRIO (1,bytesLength))
+    charVal <- replicateM times (randomRIO (0,255))
+    let funcs = tupleApply replaceByte (zip location charVal)
+    let final_func = foldl (.) (head funcs) (drop 1 funcs)
+    return (final_func bytes)
+
+sortSection :: Int -> Int -> BC.ByteString -> BC.ByteString
+sortSection start size bytes = mconcat [before,changed,after]
+    where (before,rest) = BC.splitAt start bytes
+          (target,after) = BC.splitAt size rest
+          changed = BC.reverse (BC.sort target)
+
+randomSortSection :: BC.ByteString -> Int -> IO BC.ByteString
+randomSortSection bytes times = do
+    sectionSize <- replicateM times (randomRIO (10, 25))
+    let bytesLength = BC.length bytes
+    start <- mapM (randomRIO . (\x -> (1, bytesLength - x))) sectionSize
+    let funcs = tupleApply sortSection (zip start sectionSize)
+    let final_func = foldl (.) (head funcs) (drop 1 funcs)
+    return (final_func bytes)
+
+tupleApply f (x:xs) = if xs == []
+    then []
+    else (f (fst x) (snd x)) : tupleApply f xs
