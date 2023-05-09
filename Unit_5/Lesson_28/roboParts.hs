66d8b1a3b37984221b30996adb34203c71fd5319
imaldonado <imaldonado@wiseconn.com>
2023-05-08 22:06:13 -0400

    Update

--- Unit_5/Lesson_28/roboParts.hs
+++ Unit_5/Lesson_28/roboParts.hs
@@ -1,7 +1,7 @@
+{-# LANGUAGE OverloadedStrings #-}
 import Data.Map qualified as Map
 import Data.Text.Lazy qualified as T
 import Data.Text.Lazy.IO qualified as TIO
-import GHC.IO.Device qualified as T
 import Text.Read qualified as TR
 
 data RobotPart = RobotPart
@@ -39,11 +39,38 @@ robotHead =
       count = 2
     }
 
+robotLegs :: RobotPart
+robotLegs =
+  RobotPart
+    { name = "robot legs",
+      description = "these legs run fast",
+      cost = 7098.33,
+      count = 7
+    }
+
+robotTorso :: RobotPart
+robotTorso =
+  RobotPart
+    { name = "robot torso",
+      description = "this torso is bulletproof",
+      cost = 15000.3,
+      count = 3
+    }
+
+robotGun :: RobotPart
+robotGun =
+  RobotPart
+    { name = "robot torso",
+      description = "this torso is bulletproof",
+      cost = 15000.3,
+      count = 3
+    }
+
 partsDB :: Map.Map Int RobotPart
 partsDB = Map.fromList keyVals
   where
-    keys = [1, 2, 3]
-    vals = [leftArm, rightArm, robotHead]
+    vals = [leftArm, rightArm, robotHead, robotTorso, robotLegs, robotGun]
+    keys = [1 .. length vals]
     keyVals = zip keys vals
 
 allParts :: [RobotPart]
@@ -52,8 +79,33 @@ allParts = map snd (Map.toList partsDB)
 partVal :: Maybe RobotPart
 partVal = Map.lookup 1 partsDB
 
--- Q28.3 
+-- Q28.3
 -- Make a command-line application that has a database of various RobotParts (at least five),
 -- and then lets the user enter in two-part IDs and returns the one with the lowest cost.
 -- Handle the case of the user entering an ID that’s not in the parts database.
--- TODO: This shit
+
+main :: IO ()
+main = do
+  print "Enter two part numbers:"
+  input <- TIO.getContents
+  let lines = (T.lines input)
+  mapM_ (myIOprint . minCost . parse) lines
+
+minCost :: [Int] -> String
+minCost list =
+    if length list /= 2
+        then ""
+        else  
+
+parse :: T.Text -> Maybe [Int]
+parse line = if length split /= 2
+    then Nothing
+    else Just (map (read . T.unpack) split)
+        where split = map T.strip (T.splitOn " " line)
+
+myIOprint :: Maybe String -> IO ()
+myIOprint Nothing = return ()
+myIOprint Just string = do
+  if string == "" 
+    then return ()
+    else print string
