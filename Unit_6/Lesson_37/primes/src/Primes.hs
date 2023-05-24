f152751ed7cd0caa23aaf1841c2a38534cd0754a
imaldonado <imaldonado@wiseconn.com>
2023-05-24 14:53:51 -0400

    Update

--- Unit_6/Lesson_37/primes/src/Primes.hs
+++ Unit_6/Lesson_37/primes/src/Primes.hs
@@ -1,14 +1,16 @@
 module Primes where
 
 primes :: [Int]
-primes = [1 .. ]
+primes = [1 .. 10001]
 
 sieve :: [Int] -> [Int]
 sieve [] = []
-sieve (nextPrime:rest) = nextPrime : sieve noFactors
-    where noFactors = filter (not . (== 0) . (`mod` nextPrime)) rest
+sieve (nextPrime : rest) = nextPrime : sieve noFactors
+  where
+    noFactors = filter (not . (== 0) . (`mod` nextPrime)) rest
 
 isPrime :: Int -> Maybe Bool
-isPrime n | n < 0 = Nothing
-    | n >= length primes = Nothing
-    | otherwise = Just (n `elem` primes)
+isPrime n
+  | n < 0 = Nothing
+  | n >= length primes = Nothing
+  | otherwise = Just (n `elem` primes)
--- Unit_6/Lesson_37/primes/test/Spec.hs
+++ Unit_6/Lesson_37/primes/test/Spec.hs
@@ -1,51 +1,75 @@
-import Data.Maybe ( isJust, fromJust, isNothing )
-import Primes ( isPrime, primes )
+import Data.Maybe (fromJust, isJust, isNothing)
+import Primes (isPrime, primes)
 import Test.QuickCheck
-    ( quickCheck, quickCheckWith, stdArgs, Args(maxSuccess) )
+  ( Args (maxSuccess),
+    quickCheck,
+    quickCheckWith,
+    stdArgs,
+  )
 
 main :: IO ()
 main = do
-    quickCheck prop_validPrimesOnly
-    quickCheckWith stdArgs { maxSuccess = 1000} prop_primesArePrime
-    quickCheckWith stdArgs { maxSuccess = 1000} prop_nonPrimesAreComposite
-    quickCheck prop_factorsMakeOriginal
-    quickCheck prop_allFactorsPrime
+  quickCheck prop_validPrimesOnly
+  quickCheckWith stdArgs {maxSuccess = 1000} prop_primesArePrime
+  quickCheckWith stdArgs {maxSuccess = 1000} prop_nonPrimesAreComposite
+  quickCheck prop_factorsMakeOriginal
+  quickCheck prop_allFactorsPrime
 
 prop_validPrimesOnly :: Int -> Bool
-prop_validPrimesOnly val = if val <= 0 || val >= length primes
-        then isNothing result
-        else isJust result
-    where result = isPrime val
+prop_validPrimesOnly val =
+  if val <= 0 || val >= length primes
+    then isNothing result
+    else isJust result
+  where
+    result = isPrime val
 
 prop_primesArePrime :: Int -> Bool
-prop_primesArePrime val = result /= Just True || null divisors
-    where result = isPrime val
-          divisors = filter ((== 0) . (val `mod`)) [2 .. (val - 1)]
+prop_primesArePrime val =
+  if result == Just True
+    then length divisors == 0
+    else True
+  where
+    result = isPrime val
+    divisors = filter ((== 0) . (val `mod`)) [2 .. (val - 1)]
 
 prop_nonPrimesAreComposite :: Int -> Bool
-prop_nonPrimesAreComposite val = result /= Just False || not (null divisors)
-    where result = isPrime val
-          divisors = filter ((== 0) . (val `mod`)) [2 .. (val - 1)]
+prop_nonPrimesAreComposite val =
+  if result == Just False
+    then length divisors > 0
+    else True
+  where
+    result = isPrime val
+    divisors = filter ((== 0) . (val `mod`)) [2 .. (val - 1)]
 
 unsafePrimeFactors :: Int -> [Int] -> [Int]
 unsafePrimeFactors 0 [] = []
 unsafePrimeFactors n [] = []
-
-unsafePrimeFactors n (next:primes) = if n `mod` next == 0
-    then next:unsafePrimeFactors (n `div` next) (next:primes)
+unsafePrimeFactors n (next : primes) =
+  if n `mod` next == 0
+    then next : unsafePrimeFactors (n `div` next) (next : primes)
     else unsafePrimeFactors n primes
 
 primeFactors :: Int -> Maybe [Int]
-primeFactors n | n < 2 = Nothing
-        | n >= length primes = Nothing
-        | otherwise = Just (unsafePrimeFactors n primesLessThanN)
-    where primesLessThanN = filter (<= n) primes
+primeFactors n
+  | n < 2 = Nothing
+  | n >= length primes = Nothing
+  | otherwise = Just (unsafePrimeFactors n primesLessThanN)
+  where
+    primesLessThanN = filter (<= n) primes
 
 prop_factorsMakeOriginal :: Int -> Bool
-prop_factorsMakeOriginal val = isNothing result || product (fromJust result) == val
-    where result = primeFactors val
+prop_factorsMakeOriginal val =
+  if result == Nothing
+    then True
+    else product (fromJust result) == val
+  where
+    result = primeFactors val
 
 prop_allFactorsPrime :: Int -> Bool
-prop_allFactorsPrime val = isNothing result || all (== Just True) resultsPrime
-    where result = primeFactors val
-          resultsPrime = map isPrime (fromJust result)
+prop_allFactorsPrime val =
+  if result == Nothing
+    then True
+    else all (== Just True) resultsPrime
+  where
+    result = primeFactors val
+    resultsPrime = map isPrime (fromJust result)
