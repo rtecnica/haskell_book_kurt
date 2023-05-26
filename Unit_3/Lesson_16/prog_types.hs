5ead02f0e50723bd3b9d40f2ff980e24f9040361
rtecnica <imaldonado@ing.uchile.cl>
2023-05-25 22:03:22 -0400

    Update

--- Unit_3/Lesson_16/prog_types.hs
+++ Unit_3/Lesson_16/prog_types.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-
-{-# HLINT ignore "Use newtype instead of data" #-}
 type FirstName = String
 
 type LastName = String
--- Unit_7/Lesson_41/db-lesson/app/Main.hs
+++ Unit_7/Lesson_41/db-lesson/app/Main.hs
@@ -1,13 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Control.Applicative
+import qualified Data.Text as T
 import Data.Time
 import Database.SQLite.Simple
 import Database.SQLite.Simple.FromRow
 
 main :: IO ()
-main = print "db-lesson"
+main = do
+  print "Enter a command"
+  command <- getLine
+  performCommand command
 
 data Tool = Tool
   { toolId :: Int,
@@ -82,3 +87,120 @@ printUsers = withConn "tools.db" $
   \conn -> do
     resp <- query_ conn "SELECT * FROM users;" :: IO [User]
     mapM_ print resp
+
+printToolQuery :: Query -> IO ()
+printToolQuery q = withConn "tools.db" $
+  \conn -> do
+    resp <- query_ conn q :: IO [Tool]
+    mapM_ print resp
+
+printTools :: IO ()
+printTools = printToolQuery "SELECT * FROM tools;"
+
+printAvailable :: IO ()
+printAvailable =
+  printToolQuery $
+    mconcat
+      [ "select * from tools ",
+        "where id not in ",
+        "(select tool_id from checkedout);"
+      ]
+
+printCheckedout :: IO ()
+printCheckedout =
+  printToolQuery $
+    mconcat
+      [ "select * from tools ",
+        "where id in ",
+        "(select tool_id from checkedout);"
+      ]
+
+selectTool :: Connection -> Int -> IO (Maybe Tool)
+selectTool conn toolId = do
+  resp <- query conn "SELECT * FROM tools WHERE id = (?)" (Only toolId) :: IO [Tool]
+  return $ firstOrNothing resp
+
+firstOrNothing :: [a] -> Maybe a
+firstOrNothing [] = Nothing
+firstOrNothing (x : _) = Just x
+
+updateTool :: Tool -> Day -> Tool
+updateTool tool date =
+  tool
+    { lastReturned = date,
+      timesBorrowed = 1 + timesBorrowed tool
+    }
+
+updateOrWarn :: Maybe Tool -> IO ()
+updateOrWarn Nothing = print "id not found"
+updateOrWarn (Just tool) = withConn "tools.db" $
+  \conn -> do
+    let q = mconcat ["UPDATE TOOLS SET ", "lastReturned = ?,", " timesBorrowed = ? ", "WHERE ID = ?;"]
+    execute
+      conn
+      q
+      ( lastReturned tool,
+        timesBorrowed tool,
+        toolId tool
+      )
+    print "tool updated"
+
+updateToolTable :: Int -> IO ()
+updateToolTable toolId = withConn "tools.db" $
+  \conn ->
+    do
+      tool <- selectTool conn toolId
+      currentDay <- utctDay <$> getCurrentTime
+      let updatedTool = updateTool <$> tool <*> pure currentDay
+      updateOrWarn updatedTool
+
+instance ToRow Tool where
+  toRow tool =
+    [ SQLInteger $ fromIntegral $ toolId tool,
+      SQLText $ T.pack $ name tool,
+      SQLText $ T.pack $ description tool,
+      SQLText $ T.pack $ show $ lastReturned tool,
+      SQLInteger $ fromIntegral $ timesBorrowed tool
+    ]
+
+checkin :: Int -> IO ()
+checkin toolId = withConn "tools.db" $
+  \conn -> do
+    execute conn "DELETE FROM checkedout WHERE tool_id = (?);" (Only toolId)
+
+checkinAndUpdate :: Int -> IO ()
+checkinAndUpdate toolId = do
+  checkin toolId
+  updateToolTable toolId
+
+promptAndAddUser :: IO ()
+promptAndAddUser = do
+  print "Enter new user name"
+  userName <- getLine
+  addUser userName
+
+promptAndCheckout :: IO ()
+promptAndCheckout = do
+  print "Enter the id of the user"
+  userId <- pure read <*> getLine
+  print "Enter the id of the tool"
+  toolId <- pure read <*> getLine
+  checkout userId toolId
+
+promptAndCheckin :: IO ()
+promptAndCheckin = do
+  print "enter the id of tool"
+  toolId <- pure read <*> getLine
+  checkinAndUpdate toolId
+
+performCommand :: String -> IO ()
+performCommand command
+  | command == "users" = printUsers >> main
+  | command == "tools" = printTools >> main
+  | command == "adduser" = promptAndAddUser >> main
+  | command == "checkout" = promptAndCheckout >> main
+  | command == "checkin" = promptAndCheckin >> main
+  | command == "in" = printAvailable >> main
+  | command == "out" = printCheckedout >> main
+  | command == "quit" = print "bye!"
+  | otherwise = print "Sorry command not found" >> main
